9befb178c4dbc0faef0e2183f3dd3ca968985250
jiya0308 <jiyamishra0304@gmail.com>
2026-01-12 15:18:56 +0530

    Add palindrome check in palindrome.cpp

---
+++ Strings-> palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    string rev = "";
+
+    for(int i=s.length()-1;i>=0;i--)
+        rev += s[i];
+
+    if(s == rev)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
